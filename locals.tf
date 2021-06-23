44d2eb681ed91254dd344e60a394b114a43f058c
ramsety <86119207+ramsety@users.noreply.github.com>
2021-06-23 02:17:23 -0400

    Update locals.tf

--- locals.tf
+++ locals.tf
@@ -2,5 +2,4 @@ locals {
   environment_name        = var.environment_name
   resource_group_name     = "rg-test-${local.environment_name}-001"
   location                = "eastus"  
-  subnet_ids              = "10.0.0.0/24"
 }
