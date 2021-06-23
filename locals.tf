ac67b7a7823c6dda27875e606d5aba2c332c36ba
ramsety <86119207+ramsety@users.noreply.github.com>
2021-06-23 01:51:58 -0400

    Update locals.tf

--- locals.tf
+++ locals.tf
@@ -1,7 +1,10 @@
 locals {
   environment_name        = var.environment_name
   resource_group_name     = "rg-test-${local.environment_name}-001"
-  container_registry_name = var.container_registry_name
   location                = "eastus"
-  allowed_virtual_networks = var.allowed_subnet_ids 
+  allowed_virtual_networks = [ for s in var.allowed_subnet_ids : {
+    action = "Allow"
+    subnet_id = "s"
+  }]
+    
 }
