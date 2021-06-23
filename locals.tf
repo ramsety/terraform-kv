0aec15d18e8c4cf1f1a82e427d5bf58332c68c77
ramsety <86119207+ramsety@users.noreply.github.com>
2021-06-23 01:41:36 -0400

    Update locals.tf

--- locals.tf
+++ locals.tf
@@ -4,7 +4,7 @@ locals {
   container_registry_name = var.container_registry_name
   location                = "eastus"
 
-  allowed_virtual_networks = [for s in var.allowed_subnet_ids : {
+  allowed_virtual_networks = [var.allowed_subnet_ids : {
     action    = "Allow",
     subnet_id = s
   }]
