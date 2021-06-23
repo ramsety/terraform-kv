527dbe62b1a9791cc8db3e0f44fc361ecdbb290d
ramsety <86119207+ramsety@users.noreply.github.com>
2021-06-23 01:41:44 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,3 +1,15 @@
+variable "KV_NAME" {
+    description = "Value of the  KeyVault"
+    type = string
+    default = "test"
+}
+
+variable "RG_NAME" {
+    description = "Value of the resource group"
+    type = string
+    default = "test"
+}
+
 variable "environment_name" {
     description = "Value of the environment where acr will be deployed"
     type = string
